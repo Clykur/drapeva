df1afa583d6bd7d807ba9a7c1eeaacf19fc0d16b
Karthik Naramala <karthiknaramala9949@gmail.com>
2026-07-04 01:18:48 +0530

    Workflow and tests

---
+++ frontend/__tests__/math.test.ts
@@ -0,0 +1,7 @@
+import { describe, it, expect } from "vitest";
+
+describe("Frontend tests", () => {
+  it("should successfully execute a simple math test", () => {
+    expect(2 + 2).toBe(4);
+  });
+});
